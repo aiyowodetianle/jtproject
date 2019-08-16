03ab533ac1c3c19bc0999d3a03b427f75871824e
tarena <tarena@DESKTOP-B05H0G8>
2019-08-16 08:56:37 +0800

    1day

---
+++ mh/mh-common/src/main/java/com/mh/pojo/BasePojo.java
@@ -0,0 +1,15 @@
+package com.mh.pojo;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import lombok.Data;
+import lombok.experimental.Accessors;
+
+@Data
+@Accessors(chain = true)
+public class BasePojo  implements Serializable{
+	
+	private Date created;
+	private Date updated;
+}
